a9c1f3bbc69e22f619ed5685b74c3ab35ea06493
Rob Prouse <rob.prouse@bentley.com>
2017-10-03 10:42:21 -0400

    Initial import of default template

---
+++ IdeaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdeaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
