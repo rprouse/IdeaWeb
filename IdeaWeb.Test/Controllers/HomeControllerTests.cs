a96a2f5039192359a16eb77f3bda2f397ecd5ee8
Rob Prouse <rob.prouse@bentley.com>
2017-10-03 11:21:09 -0400

    Added a unit test project

---
+++ IdeaWeb.Test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IdeaWeb.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace IdeaWeb.Test
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        HomeController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new HomeController();
+        }
+
+        [Test]
+        public void IndexReturnsIdeasView()
+        {
+            IActionResult result = _controller.Index();
+
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
