bee712d0a48cd1c9e424bc6630febeaa04fbbf89
Rob Prouse <rob@prouse.org>
2017-10-09 19:05:04 -0400

    Scaffolded editing, viewing, creating and deleting ideas

---
+++ IdeaWeb/Controllers/IdeasController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IdeaWeb.Data;
+using IdeaWeb.Models;
+
+namespace IdeaWeb.Controllers
+{
+    public class IdeasController : Controller
+    {
+        private readonly IdeaContext _context;
+
+        public IdeasController(IdeaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ideas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var idea = await _context.Ideas
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+
+            return View(idea);
+        }
+
+        // GET: Ideas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ideas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Rating")] Idea idea)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(idea);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(idea);
+        }
+
+        // GET: Ideas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var idea = await _context.Ideas.SingleOrDefaultAsync(m => m.Id == id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+            return View(idea);
+        }
+
+        // POST: Ideas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Rating")] Idea idea)
+        {
+            if (id != idea.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(idea);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IdeaExists(idea.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(idea);
+        }
+
+        // GET: Ideas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var idea = await _context.Ideas
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+
+            return View(idea);
+        }
+
+        // POST: Ideas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var idea = await _context.Ideas.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Ideas.Remove(idea);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool IdeaExists(int id)
+        {
+            return _context.Ideas.Any(e => e.Id == id);
+        }
+    }
+}
