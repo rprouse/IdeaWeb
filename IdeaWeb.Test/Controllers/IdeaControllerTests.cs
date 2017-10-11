70cb6d9707d0dcc219da4d105e975e227307975c
Rob Prouse <rob@prouse.org>
2017-10-10 22:28:01 -0400

    Added initial unit tests for the IdeasController

--- IdeaWeb.Test/Controllers/IdeaControllerTests.cs
+++ IdeaWeb.Test/Controllers/IdeaControllerTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using IdeaWeb.Controllers;
 using IdeaWeb.Data;
 using IdeaWeb.Models;
@@ -8,32 +6,40 @@ using IdeaWeb.Test.Data;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 
-namespace IdeaWeb.Test
+namespace IdeaWeb.Test.Controllers
 {
     [TestFixture]
-    public class HomeControllerTests
+    public class IdeaControllerTests
     {
         const int NUM_IDEAS = 10;
 
-        HomeController _controller;
+        IdeasController _controller;
 
         [SetUp]
         public void SetUp()
         {
             IdeaContext context = TestIdeaContextFactory.Create(TestContext.CurrentContext, NUM_IDEAS);
-            _controller = new HomeController(context);
+            _controller = new IdeasController(context);
+        }
+
+        [TestCase(null)]
+        [TestCase(100)]
+        public async Task DetailsWithNullOrMissingIdReturnsNotFound(int? id)
+        {
+            IActionResult result = await _controller.Details(id);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
         [Test]
-        public async Task IndexReturnsListOfIdeas()
+        public async Task DetailsReturnsIdea()
         {
-            ViewResult result = await _controller.Index() as ViewResult;
+            ViewResult result = await _controller.Details(1) as ViewResult;
 
             Assert.That(result?.Model, Is.Not.Null);
-            Assert.That(result.Model, Has.Count.EqualTo(NUM_IDEAS));
 
-            IEnumerable<Idea> ideas = result.Model as IEnumerable<Idea>;
-            Idea idea = ideas?.FirstOrDefault();
+            Idea idea = result.Model as Idea;
             Assert.That(idea, Is.Not.Null);
             Assert.That(idea.Name, Is.EqualTo("Idea name 1"));
             Assert.That(idea.Description, Does.Contain("1"));
---
+++ IdeaWeb.Test/Data/TestIdeaContextFactory.cs
@@ -0,0 +1,43 @@
+﻿using IdeaWeb.Data;
+using IdeaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace IdeaWeb.Test.Data
+{
+    public static class TestIdeaContextFactory
+    {
+        /// <summary>
+        /// Creates an <see cref="IdeaContext"/> using an in-memory
+        /// database and populates it with test data.
+        /// </summary>
+        /// <param name="numIdeas">The number of ideas you want to be created in the database</param>
+        /// <param name="testContext">The NUnit TestContext</param>
+        /// <returns>A test context backed by an in-memory database</returns>
+        public static IdeaContext Create(TestContext testContext, int numIdeas)
+        {
+            // Create unique database names based on the test id
+            var options = new DbContextOptionsBuilder<IdeaContext>()
+                .UseInMemoryDatabase(testContext.Test.ID)
+                .Options;
+
+            // Seed the in-memory database
+            using (var context = new IdeaContext(options))
+            {
+                for (int i = 1; i <= numIdeas; i++)
+                {
+                    context.Add(new Idea
+                    {
+                        Name = $"Idea name {i}",
+                        Description = $"Description {i}",
+                        Rating = i % 3 + 1
+                    });
+                }
+                context.SaveChanges();
+            }
+
+            // Use a clean copy of the context within the tests
+            return new IdeaContext(options);
+        }
+    }
+}
