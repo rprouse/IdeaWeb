1db15b42efaa0294b856770f3d820393678557ae
Rob Prouse <rob@prouse.org>
2017-10-13 19:42:23 -0400

    Finished unit tests for the idea controller

--- IdeaWeb.Test/Controllers/IdeaControllerTests.cs
+++ IdeaWeb.Test/Controllers/IdeaControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using IdeaWeb.Controllers;
 using IdeaWeb.Data;
@@ -56,17 +57,7 @@ namespace IdeaWeb.Test.Controllers
 
             ViewResult result = await _controller.Details(expected.Id) as ViewResult;
 
-            Assert.That(result?.Model, Is.Not.Null);
-
-            Idea idea = result.Model as Idea;
-            Assert.That(idea, Is.Not.Null);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(idea.Name, Is.EqualTo(expected.Name));
-                Assert.That(idea.Description, Is.EqualTo(expected.Description));
-                Assert.That(idea.Rating, Is.EqualTo(expected.Rating));
-            });
+            AssertViewResultContainsExpectedId(expected, result);
         }
 
         [Test]
@@ -103,7 +94,7 @@ namespace IdeaWeb.Test.Controllers
         }
 
         [Test]
-        public async Task CreateMissingNameDoesNotPersist()
+        public async Task CreateWithInvalidModelDoesNotPersist()
         {
             var idea = new Idea
             {
@@ -120,7 +111,7 @@ namespace IdeaWeb.Test.Controllers
         }
 
         [Test]
-        public async Task CreateMissingNameReturnsUnpersistedModel()
+        public async Task CreateWithInvalidModelUnpersistedModel()
         {
             var idea = new Idea
             {
@@ -132,6 +123,183 @@ namespace IdeaWeb.Test.Controllers
 
             Assert.That(result?.Model, Is.Not.Null);
             Assert.That(result.Model, Is.EqualTo(idea).Using(new IdeaEqualityComparer()));
+        }
+
+
+        [TestCase(null)]
+        [TestCase(-1)]
+        [TestCase(10000000)]
+        public async Task EditGetWithNullOrMissingIdReturnsNotFound(int? id)
+        {
+            IActionResult result = await _controller.Edit(id);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditGetReturnsIdea()
+        {
+            // Get a valid idea out of the db
+            var expected = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(expected, Is.Not.Null);
+
+            ViewResult result = await _controller.Edit(expected.Id) as ViewResult;
+
+            AssertViewResultContainsExpectedId(expected, result);
+        }
+
+
+        [TestCase(-1)]
+        [TestCase(10000000)]
+        public async Task EditPostWithNullOrMissingIdReturnsNotFound(int id)
+        {
+            var idea = new Idea
+            {
+                Id = id,
+                Name = "Invalid"
+            };
+            IActionResult result = await _controller.Edit(id, idea);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditPersistsChanges()
+        {
+            // Get a valid idea out of the db and update it
+            var idea = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(idea, Is.Not.Null);
+            idea.Description += "Updated";
+
+            await _controller.Edit(idea.Id, idea);
+
+            // Fetch the idea back out of the database
+            var updated = await _context.Ideas.SingleOrDefaultAsync(m => m.Id == idea.Id);
+            Assert.That(updated, Is.Not.Null);
+            Assert.That(updated.Description, Is.EqualTo(idea.Description));
+        }
+
+        [Test]
+        public async Task EditRedirectsToDetails()
+        {
+            // Get a valid idea out of the db and update it
+            var idea = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(idea, Is.Not.Null);
+            idea.Description = "Updated";
+
+            RedirectToActionResult result = await _controller.Edit(idea.Id, idea) as RedirectToActionResult;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.ActionName, Is.EqualTo("Details"));
+            Assert.That(result.RouteValues["Id"], Is.EqualTo(idea.Id));
+        }
+
+        [Test]
+        public async Task EditWithInvalidModelDoesNotSave()
+        {
+            // Get a valid idea out of the db and update it
+            var idea = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(idea, Is.Not.Null);
+
+            // In-memory database, work on a copy
+            var copy = new Idea
+            {
+                Id = idea.Id,
+                Description = idea.Description + "Updated"
+            };
+
+            _controller.ModelState.AddModelError("TestError", "TestError");
+            await _controller.Edit(copy.Id, copy);
+
+            // Fetch the idea back out of the database
+            var updated = await _context.Ideas.SingleOrDefaultAsync(m => m.Id == idea.Id);
+            Assert.That(updated, Is.Not.Null);
+            Assert.That(updated.Description, Is.Not.EqualTo(copy.Description));
+        }
+
+        [Test]
+        public async Task EditWithDifferentIdReturnsNotFound()
+        {
+            // Get a valid idea out of the db and update it
+            var idea = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(idea, Is.Not.Null);
+
+            IActionResult result = await _controller.Edit(10000, idea);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task EditWithInvalidIdReturnsNotFound()
+        {
+            // Get a valid idea out of the db and update it
+            var idea = new Idea
+            {
+                Id = 10000,
+                Name = "Invalid",
+                Description = "Doesn't exist"
+            };
+
+            IActionResult result = await _controller.Edit(idea.Id, idea);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+
+        [TestCase(null)]
+        [TestCase(-1)]
+        [TestCase(10000000)]
+        public async Task DeleteWithNullOrMissingIdReturnsNotFound(int? id)
+        {
+            IActionResult result = await _controller.Delete(id);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public async Task DeleteReturnsIdea()
+        {
+            // Get a valid idea out of the db
+            var expected = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(expected, Is.Not.Null);
+
+            ViewResult result = await _controller.Delete(expected.Id) as ViewResult;
+
+            AssertViewResultContainsExpectedId(expected, result);
+        }
+
+        [Test]
+        public async Task DeleteConfirmedDeletesIdea()
+        {
+            // Get a valid idea out of the db
+            var idea = await _context.Ideas.FirstOrDefaultAsync();
+            Assume.That(idea, Is.Not.Null);
+
+            await _controller.DeleteConfirmed(idea.Id);
+
+            // Fetch the idea back out of the database
+            var result = await _context.Ideas.SingleOrDefaultAsync(i => i.Id == idea.Id);
+            Assert.That(result, Is.Null);
+        }
+
+        private static void AssertViewResultContainsExpectedId(Idea expected, ViewResult result)
+        {
+            Assert.That(result?.Model, Is.Not.Null);
+
+            Idea idea = result.Model as Idea;
+            Assert.That(idea, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(idea.Name, Is.EqualTo(expected.Name));
+                Assert.That(idea.Description, Is.EqualTo(expected.Description));
+                Assert.That(idea.Rating, Is.EqualTo(expected.Rating));
+            });
         }
     }
 }
