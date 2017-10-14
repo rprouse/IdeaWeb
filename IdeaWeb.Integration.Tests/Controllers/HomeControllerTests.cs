df68a7745122fba6d6cc9d888b0e0d6b60cbb4bc
Rob Prouse <rob@prouse.org>
2017-10-13 20:38:19 -0400

    Initial integration test project

---
+++ IdeaWeb.Integration.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+
+namespace IdeaWeb.Integration.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        TestServer _server;
+        HttpClient _client;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+
+            _client = _server.CreateClient();
+        }
+
+        [Test]
+        public async Task HomePageContainsIdeas()
+        {
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.That(response, Does.Contain("<span class=\"name\">"));
+        }
+    }
+}
