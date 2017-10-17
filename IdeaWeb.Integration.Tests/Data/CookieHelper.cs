458575092473419279a0c4ab001570f36ed1134a
Rob Prouse <rob@prouse.org>
2017-10-16 23:05:50 -0400

    Added doc comments

--- IdeaWeb.Integration.Tests/Data/CookieHelper.cs
+++ IdeaWeb.Integration.Tests/Data/CookieHelper.cs
@@ -11,13 +11,28 @@ namespace IdeaWeb.Integration.Tests.Data
 {
     public static class CookieHelper
     {
+        /// <summary>
+        /// Copies the cookies from a <see cref="HttpResponseMessage"/> into a new <see cref="HttpContent"/>
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="response"></param>
         public static void CopyCookiesFromResponse(this HttpContent request, HttpResponseMessage response) =>
             request.AddCookies(response.ExtractCookies());
 
+        /// <summary>
+        /// Extracts the cookies from a <see cref="HttpResponseMessage"/>
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
         public static IList<SetCookieHeaderValue> ExtractCookies(this HttpResponseMessage response) =>
             response.Headers.TryGetValues("Set-Cookie", out IEnumerable<string> values) ?
                 SetCookieHeaderValue.ParseList(values.ToList()) : null;
 
+        /// <summary>
+        /// Adds the given cookies to a <see cref="HttpContent"/> request.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cookies"></param>
         public static void AddCookies(this HttpContent request, IList<SetCookieHeaderValue> cookies) =>
             cookies.Where(c => c.Name.HasValue && c.Value.HasValue)
                 .Select(c => new CookieHeaderValue(c.Name.Value, c.Value.Value).ToString())
